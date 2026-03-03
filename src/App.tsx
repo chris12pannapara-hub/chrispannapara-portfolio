1fb91e62695b18ef730f8806430b1733c9eb9681
Chris <chris12pannapara@gmail.com>
2026-03-03 10:31:36 -0500

    feat(chore): Adding minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
-import Footer from './components/Footer';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './styles/globals.scss';
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
       <Experience />
       <Projects />
       <Resume />
-      <Contact /> 
+      <Contact />
       <Footer />
     </div>
   );
